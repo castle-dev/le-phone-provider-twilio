af14779104fc7ea20d134f01031aa1d667eb0861
Scott Lowe <scott@entercastle.com>
2015-05-18 14:47:10 -0400

    docs(PhoneProvider): document constructor and `text` method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 var q = require('q');
 var twilio = require('twilio');
-
+/**
+ * A bridge between le-phone-service and Twilio
+ * @class PhoneProvider
+ * @param {string} accountSID the twilio account SID
+ * @param {string} authToken the twilio auth token
+ * @returns {service}
+ */
 var PhoneProvider = function (accountSID, authToken) {
   if (!accountSID) { throw new Error('Account SID required'); }
   if (!authToken) { throw new Error('Auth token required'); }
   var _api = twilio(accountSID, authToken);
-
+  /**
+   * Sends a text message
+   * @function text
+   * @memberof PhoneProvider
+   * @instance
+   * @param {string} from the phone number of the sender
+   * @param {string} to the phone number of the recipient
+   * @param {string} message the content of the text message
+   * @returns {promise}
+   */
   this.text = function (from, to, message) {
     if (!from) { return q.reject(new Error('From phone number required')); }
     if (!to) { return q.reject(new Error('To phone number required')); }
